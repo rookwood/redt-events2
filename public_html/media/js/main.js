acd2a232cd22dc406ae3c88693ccd309de59f661
Matt Hollis <somehollis@gmail.com>
2012-11-04 17:27:59 -0600

    Event viewing-related ajax

--- public_html/media/js/main.js
+++ public_html/media/js/main.js
@@ -3,6 +3,10 @@ if (typeof jQuery != "undefined"){
 	// On DOM Ready
 	(function($){$(document).ready(function(){
 
+		/**
+		 * Notices
+		 */
+		
 		// Show closing "X"s
 		$("div.notice-close").show();
 		//$('div.notice').width($('div.notice').width());
@@ -27,36 +31,62 @@ if (typeof jQuery != "undefined"){
 			return false;
 		});
 
-		// Extending the jQuery object to add notices
-		// Example: $('div.notices-container').add_notice('success', 'You have succeeded!', true);
-		jQuery.fn.add_notice = function(type, message, persist){
+	/**
+	 * Event details loading via ajax
+	 */
+	
+	// Ajax loading of event details
+	// can't figure out why ul.header won't work for event binding
+	// so using pointless class for the moment but this is problematic
+	$('.clickable').click(function() {
 
-			var container = $(this);
+		// Find container element for our event data
+		var event_data = $(this).find('section.event_data');
 
-			var ajax_url = '/notice/add';
-			if (type != undefined){
-				ajax_url += '/'+escape(type);
-				if (message != undefined){
-					ajax_url += '/'+escape(message);
-					if (persist == true){
-						ajax_url += '/TRUE';
-					}
-				}
-			}
+		// See if we've already loaded data for this event
+		if ( ! $(this).hasClass('loaded'))
+		{
+			// save reference to clicked element as a starting point for all traversal
+			var that = this;
 
-			$.ajax({
-				url: ajax_url,
-				cache: false,
-				dataType: 'json',
-				success: function(response){
-					if (response.status == "success"){
-						container.append(response.data);
-						container.find("div.notice-close:last").show();
-					}
-				},
-				error: $.noop()
+			// ajax call to fetch event data
+			$.get($(this).data('url'), function(data) {
+				event_data.html(data);
+
+				// setup for jquery ui tabs
+				event_data.find('#tabs').tabs();
+
+				// mark event has having all data loaded
+				$(that).addClass('loaded');
+
+				$(that).toggleClass('event_collapsed event_expanded');
+
+				event_data.slideDown('slow');
 			});
-		};
+		}
+		else
+		{
+			// Toggle details display
+			if ($(this).hasClass('event_collapsed'))
+			{
+				$(this).toggleClass('event_collapsed event_expanded');
+
+				event_data.slideDown('slow');
+			}
+			else
+			{
+				var that = this;
+
+				event_data.slideUp('slow', function() {
+					$(that).toggleClass('event_collapsed event_expanded');
+				});
+			}
+		}
+	});
+
+	$('.clickable > section').click(function(e) {
+		e.stopPropagation();
+	});		
 
 	})})(jQuery); // Prevent conflicts with other js libraries
 }
