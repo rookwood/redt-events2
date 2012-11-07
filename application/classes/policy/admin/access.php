5cdb469425ea7b1962fb72e2da86b4f2f4389aa5
Matt Hollis <somehollis@gmail.com>
2012-11-06 18:43:57 -0600

    Initial admin policy framework

---
+++ application/classes/policy/admin/access.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Policy_Admin_Acess extends Policy {
+
+	const NOT_ALLOWED = 1;
+	
+	public function execute(Model_ACL_User $user, array $extras = NULL)
+	{
+		if ($user->is_an('admin') or $user->is_an('officer'))
+		{
+			return TRUE;
+		}
+		
+		return self::NOT_ALLOWED;
+	}
+
+}
